bdc01203570b38add6f78ff677779d77d839161c
BWilliams22 <1williamreb2@hdsb.ca>
2020-07-22 13:46:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+
+number1 = int(input("Write a number: "))
+number2 = int(input("Write another number: "))
+
+def calc(number1, number2): 
+ sum = number1 + number2 
+ return sum 
+
+sum = calc(number1, number2)
+print(sum)
